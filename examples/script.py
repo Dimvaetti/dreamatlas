2a233de2de3d0859bf0d23e1d56f8898cbf26579
Dimvaetti <287117788+Dimvaetti@users.noreply.github.com>
2026-05-26 23:39:21 -0500

    Updated examples/script.py with a minimal example of how to generate maps with scripting.

--- examples/script.py
+++ examples/script.py
@@ -10,11 +10,12 @@ settings.load_file(filename=PATH / 'examples' / '12_player_ea_test.dream')
 
 smackdown_map = generator_dreamatlas(settings=settings)
 
-# Make the files
+# Make the files, default location is $REPO_DIR/maps/$NAME
 smackdown_map.map_title = [None, name, name+'_plane2']
-smackdown_map.publish(location=str(PATH), name=name)
+smackdown_map.publish(location=None, name=name)
 
-# Plot map images
-smackdown_map.layout.plot()
-# smackdown_map.plot()
-plt.show()
+
+# Plot map images (broken)
+#smackdown_map.layout.plot() #TODO: fix this?
+#smackdown_map.plot()
+#plt.show()
